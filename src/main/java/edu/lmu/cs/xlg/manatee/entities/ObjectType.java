34e3e2669e9b7e28762e5424a07b7a501d19b723
Tyler Nichols <tyler.n.89@gmail.com>
2012-04-21 14:37:30 -0700

    Implemented and tested ObjectType analysis.

--- src/main/java/edu/lmu/cs/xlg/manatee/entities/ObjectType.java
+++ src/main/java/edu/lmu/cs/xlg/manatee/entities/ObjectType.java
@@ -1,9 +1,8 @@
 package edu.lmu.cs.xlg.manatee.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/*import src.main.java.edu.lmu.cs.xlg.manatee.entities.Subroutine;
-import src.main.java.edu.lmu.cs.xlg.manatee.entities.SymbolTable;*/
 import edu.lmu.cs.xlg.util.Log;
 
 public class ObjectType extends Type {
@@ -11,15 +10,16 @@ public class ObjectType extends Type {
     
     public static class Property extends Entity {
         String name;
-        String type;
+        Type type;
         
         public void analyze(Log log, SymbolTable table, Subroutine owner, boolean inLoop) {
-            //this.type.analyze(log, table, owner, inLoop);
+            // This does absolutely nothing since the Type analyze method is empty
+            this.type.analyze(log, table, owner, inLoop);
         }
         
         public Property(String name, String type) {
             this.name = name;
-            this.type = type;
+            this.type = new Type(type);
         }
         
         /*
@@ -27,6 +27,17 @@ public class ObjectType extends Type {
          * new ObjectType.Property(propertyName.image,
             propertyType)
          */
+    }
+    
+    public void analyze(Log log, SymbolTable table, Subroutine owner, boolean inLoop) {
+        ArrayList<String> history = new ArrayList<String>();
+        for (Property p: this.properties) {
+            if (history.contains(p.name)) {
+                log.error("Duplicate property IDs in ObjectType.");
+            } else {
+                history.add(p.name);
+            }
+        }
     }
     
     public ObjectType(String name, List<Property> properties) {
