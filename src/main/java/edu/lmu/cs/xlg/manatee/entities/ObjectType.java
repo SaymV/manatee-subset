f961159aa4c4e0da7a37adc104f47477ddb7ee3d
Tyler Nichols <tyler.n.89@gmail.com>
2012-04-21 13:53:51 -0700

    Minor modifications

--- src/main/java/edu/lmu/cs/xlg/manatee/entities/ObjectType.java
+++ src/main/java/edu/lmu/cs/xlg/manatee/entities/ObjectType.java
@@ -7,31 +7,31 @@ import src.main.java.edu.lmu.cs.xlg.manatee.entities.SymbolTable;*/
 import edu.lmu.cs.xlg.util.Log;
 
 public class ObjectType extends Type {
-	List<Property> properties;
-	
-	public static class Property extends Entity {
-		String name;
-		String p;
-		
-		public void analyze(Log log, SymbolTable table, Subroutine owner, boolean inLoop) {
-			//p.analyze(log, table, owner, inLoop);
-	    }
-		
-		public Property(String name, String p) {
-			this.name = name;
-			this.p = p;
-		}
-		
-		/*
-		 * 
-		 * new ObjectType.Property(propertyName.image,
+    List<Property> properties;
+    
+    public static class Property extends Entity {
+        String name;
+        String type;
+        
+        public void analyze(Log log, SymbolTable table, Subroutine owner, boolean inLoop) {
+            //this.type.analyze(log, table, owner, inLoop);
+        }
+        
+        public Property(String name, String type) {
+            this.name = name;
+            this.type = type;
+        }
+        
+        /*
+         * 
+         * new ObjectType.Property(propertyName.image,
             propertyType)
-		 */
-	}
-	
-	public ObjectType(String name, List<Property> properties) {
-		super(name);
-		this.properties = properties;
-	}
+         */
+    }
+    
+    public ObjectType(String name, List<Property> properties) {
+        super(name);
+        this.properties = properties;
+    }
 
 }
